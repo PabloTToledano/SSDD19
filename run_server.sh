b705694eefd2a00e31a25c9c7c24dcd78245f11d
PabloTToledano <pablotomas.toledano@alu.uclm.es>
2019-11-20 21:17:02 +0100

    new run server

--- run_server.sh
+++ run_server.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 #Lo que hay que hacer es recoger la salida del downloader para luego poder meterla como entrada del orchestrator
-proxy_downloader=$(./Downloader.py --Ice.Config="Server.config")
-echo "$proxy_downloader"
+PROXY=$(tempfile)
+./Downloader.py --Ice.Config=Server.config>$PROXY 
+
+sleep 1
+
+./orchestrator.py --Ice.Config=Server.config "$(cat $PROXY)"
+
+
