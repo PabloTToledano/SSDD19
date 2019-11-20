f327c2790743523d58bd73e27881c691bec7a983
PabloTToledano <pablotomas.toledano@alu.uclm.es>
2019-11-20 21:24:20 +0100

    added kill to run server

--- run_server.sh
+++ run_server.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #Lo que hay que hacer es recoger la salida del downloader para luego poder meterla como entrada del orchestrator
 PROXY=$(tempfile)
-./Downloader.py --Ice.Config=Server.config>$PROXY 
+./Downloader.py --Ice.Config=Server.config>$PROXY &
+PROCESO=$!
 
 sleep 1
 
 ./orchestrator.py --Ice.Config=Server.config "$(cat $PROXY)"
 
+kill -KILL $PROCESO
 
