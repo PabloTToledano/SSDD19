8f083850c14bf42f49588bbf6a2a88876c049486
Alejandro <peditito@gmail.com>
2019-12-09 19:26:43 +0100

    First try to downloader and orchestrator

--- orchestrator.py
+++ orchestrator.py
@@ -9,42 +9,73 @@ import TrawlNet
 class Orchestrator(TrawlNet.Orchestrator):
 
     downloader = None
+    fileList = {}
 
     def downloadTask(self, url, current=None):
         print(url)
         sys.stdout.flush()
         if self.downloader is not None:
-            self.downloader.addDownloadTask(url)
+            return self.downloader.addDownloadTask(url)
     
+    def write(self, message, current=None):
+        fileList[message.name]=message.hash
+        sys.stdout.flush()
     
-
 class Server(Ice.Application):
-    def run(self, argv):
-        broker = self.communicator()
-        servant = Orchestrator()
 
+def get_topic_manager(self):
+        key = 'IceStorm.TopicManager.Proxy'
+        proxy = self.communicator().propertyToProxy(key)
+        if proxy is None:
+            print("property '{}' not set".format(key))
+            return None
+
+        print("Using IceStorm in: '%s'" % key)
+        return IceStorm.TopicManagerPrx.checkedCast(proxy)
+
+
+    def run(self, argv):
+        #Parte del servidor
         adapter = broker.createObjectAdapter("OrchestratorAdapter")
         proxy = adapter.add(servant, broker.stringToIdentity("Orchestrator1"))
-
         print(proxy, flush=True)
-
         proxyServer = self.communicator().stringToProxy(argv[1])
-        print(argv[1])
+
+        #Parte de canal
+        topic_mgr = self.get_topic_manager()
+        if not topic_mgr:
+            print("Invalid proxy")
+            return 2
+
+        broker = self.communicator()
+        servant = Orchestrator()        
+       
+        subscriber = adapter.addWithUUID(servant)   
+
+        topic_name = "UpdateEvents"
+        qos = {}     
+
+        try:
+            topic = topic_mgr.retrieve(topic_name)
+        except IceStorm.NoSuchTopic:
+            topic = topic_mgr.create(topic_name)
+        
+        topic.subscribeAndGetPublisher(qos, subscriber)
+
+        print("Waiting events... '{}'".format(subscriber))        
 
         downloader = TrawlNet.DownloaderPrx.checkedCast(proxyServer)
 
         if not downloader:
             raise RuntimeError('Invalid proxy')
 
-        servant.downloader= downloader
-
+        servant.downloader = downloader
 
         adapter.activate()
         self.shutdownOnInterrupt()
         broker.waitForShutdown()
 
+        topic.unsubscribe(subscriber)
+
         return 0
-
-
-server = Server()
-sys.exit(server.main(sys.argv))
+sys.exit(Subscriber().main(sys.argv))
